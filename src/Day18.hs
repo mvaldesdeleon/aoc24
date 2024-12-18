240b576462947b4c6413778f22fb669a7c8cc9ed
Martín Valdés de León <mvdl@amazon.de>
2024-12-18 21:36:43 +0100

    feat(day18): day 18, both parts

--- src/Day18.hs
+++ src/Day18.hs
@@ -7,7 +7,6 @@ import Control.Monad
 import Control.Monad.Loops
 import Control.Monad.ST
 import Data.Array.ST
-import Data.Bits
 import qualified Data.List as L
 import Data.STRef
 import qualified Data.Text as T
@@ -65,37 +64,24 @@ simulate bs n =
     corryptedByte :: Byte -> (Int, Memory)
     corryptedByte (Byte (x, y)) = (fromInteger $ y * memorySize + x, Corrupted)
 
-display :: V.Vector Memory -> [Text]
-display v =
-  let txt = T.pack $ toChar <$> V.toList v
-   in T.chunksOf (fromInteger memorySize) txt
-  where
-    toChar Safe = '.'
-    toChar Corrupted = '#'
-
-tag :: (Show a) => String -> a -> a
-tag label value = trace (label ++ show value) value
-
 shortestPath :: V.Vector Memory -> Integer
 shortestPath memory = runST $ do
   -- the map
   tilesArr <- newListArray (0, V.length memory - 1) (toChar <$> V.toList memory) :: ST s (STUArray s Int Char)
   -- the result structures
   scoresArr <- newArray (0, V.length memory - 1) (-1) :: ST s (STUArray s Int Int)
-  parentsArr <- newArray (0, V.length memory - 1) 0 :: ST s (STUArray s Int Int8)
   -- iteration structures
-  candidatesArr <- newArray (0, V.length memory - 1) 0 :: ST s (STUArray s Int Int)
-  candidatesRef <- newSTRef 0
+  candidatesArr <- newArray (0, V.length memory - 1) False :: ST s (STUArray s Int Bool)
   doneArr <- newArray (0, V.length memory - 1) False :: ST s (STUArray s Int Bool)
   -- set initial node
   writeArray scoresArr (toInt (0, 0)) 0
-  stAdd candidatesRef candidatesArr (0, 0)
+  stAdd candidatesArr (0, 0)
 
-  whileM_ (not <$> stEmpty candidatesRef) $ do
+  whileM_ (not <$> stEmpty candidatesArr) $ do
     -- pick next best candidate
-    currPos <- stFindNext candidatesRef candidatesArr scoresArr
+    currPos <- stFindNext candidatesArr scoresArr
     -- check its neighbors. if they have not been visited, set their score, and add them to the list
-    mapM_ (stCheckNeighbor tilesArr candidatesRef candidatesArr doneArr scoresArr parentsArr currPos) [E, S, W, N]
+    mapM_ (stCheckNeighbor tilesArr candidatesArr doneArr scoresArr currPos) [E, S, W, N]
     -- mark it as visited
     writeArray doneArr (toInt currPos) True
 
@@ -113,76 +99,66 @@ shortestPath memory = runST $ do
           y = i `div` fromInteger memorySize
        in (toInteger x, toInteger y)
 
-    stEmpty :: STRef s Int -> ST s Bool
-    stEmpty candidatesRef = do
-      lastCandidate <- readSTRef candidatesRef
-      return $ lastCandidate == 0
+    stEmpty :: STUArray s Int Bool -> ST s Bool
+    stEmpty candidatesArr = do
+      candidates <- getElems candidatesArr
+      return $ not . or $ candidates
 
-    stAdd :: STRef s Int -> STUArray s Int Int -> (Integer, Integer) -> ST s ()
-    stAdd candidatesRef candidatesArr pos = do
-      lastCandidate <- readSTRef candidatesRef
-      writeArray candidatesArr lastCandidate (toInt $ tag "Added Candidate: " pos)
-      modifySTRef' candidatesRef succ
+    stAdd :: STUArray s Int Bool -> (Integer, Integer) -> ST s ()
+    stAdd candidatesArr pos = do
+      writeArray candidatesArr (toInt pos) True
 
-    stFindNext :: STRef s Int -> STUArray s Int Int -> STUArray s Int Int -> ST s (Integer, Integer)
-    stFindNext candidatesRef candidatesArr scoresArr = do
-      lastCandidate <- readSTRef candidatesRef
-      candidates <- mapM (readArray candidatesArr) [0 .. lastCandidate - 1]
+    stFindNext :: STUArray s Int Bool -> STUArray s Int Int -> ST s (Integer, Integer)
+    stFindNext candidatesArr scoresArr = do
+      candidates <- map fst . filter snd <$> getAssocs candidatesArr
       min <- minimumOnM (readArray scoresArr) candidates
       let idx = fromMaybe 0 min
-          candidateIdx = fromMaybe 0 $ L.elemIndex idx candidates
-      -- remove candidate by replacing it with the last one and shortening the list
-      last <- readArray candidatesArr (lastCandidate - 1)
-      writeArray candidatesArr candidateIdx last
-      modifySTRef' candidatesRef pred
+      writeArray candidatesArr idx False
       return $ fromInt idx
 
-    stCheckNeighbor :: STUArray s Int Char -> STRef s Int -> STUArray s Int Int -> STUArray s Int Bool -> STUArray s Int Int -> STUArray s Int Int8 -> (Integer, Integer) -> Dir -> ST s ()
-    stCheckNeighbor tilesArr candidatesRef candidatesArr doneArr scoresArr parentsArr currPos dir = do
+    stCheckNeighbor :: STUArray s Int Char -> STUArray s Int Bool -> STUArray s Int Bool -> STUArray s Int Int -> (Integer, Integer) -> Dir -> ST s ()
+    stCheckNeighbor tilesArr candidatesArr doneArr scoresArr currPos dir = do
       let nextPos = move dir currPos
       when (valid nextPos) $ do
         tile <- readArray tilesArr (toInt nextPos)
         when (tile == '.') $ do
           done <- readArray doneArr (toInt nextPos)
           unless done $ do
-            stUpdateScore scoresArr parentsArr currPos nextPos
-            stAdd candidatesRef candidatesArr nextPos
+            stUpdateScore scoresArr currPos nextPos
+            stAdd candidatesArr nextPos
 
-    stUpdateScore :: STUArray s Int Int -> STUArray s Int Int8 -> (Integer, Integer) -> (Integer, Integer) -> ST s ()
-    stUpdateScore scoresArr parentsArr currPos nextPos = do
+    stUpdateScore :: STUArray s Int Int -> (Integer, Integer) -> (Integer, Integer) -> ST s ()
+    stUpdateScore scoresArr currPos nextPos = do
       currScore <- readArray scoresArr (toInt currPos)
       oldScore <- readArray scoresArr (toInt nextPos)
       let newScore = currScore + 1
       when (oldScore == -1 || newScore <= oldScore) $ do
         writeArray scoresArr (toInt nextPos) newScore
-        -- update parents bitmask
-        oldParents <- readArray parentsArr (toInt nextPos)
-        writeArray parentsArr (toInt nextPos) (oldParents .|. moveParent currPos nextPos)
 
-    moveParent :: (Integer, Integer) -> (Integer, Integer) -> Int8
-    moveParent (x, y) (x', y') =
-      case unmove (x, y) (x', y') of
-        S -> 1
-        E -> 2
-        N -> 4
-        W -> 8
+part1 :: Text -> Integer
+part1 input =
+  let bs = parseInput input
+      mem = simulate bs 1024
+   in shortestPath mem
 
-    stParents :: STUArray s Int Int8 -> Int -> ST s [Integer]
-    stParents parentsArr idx = do
-      parents <- readArray parentsArr idx
+findFirstBadByte :: [Byte] -> Byte
+findFirstBadByte bs =
+  let s = 0
+      e = length bs - 1
+   in go s e
+  where
+    go s e
+      | s + 1 == e = bs L.!! s
+      | otherwise =
+          let m = (e - s) `div` 2 + s
+              r = shortestPath $ simulate bs (toInteger m)
+           in if r == -1 then go s m else go m e
 
-      let (x, y) = fromInt idx
-          pS = if parents .&. 1 > 0 then Just (x, y - 1) else Nothing
-          pE = if parents .&. 2 > 0 then Just (x - 1, y) else Nothing
-          pN = if parents .&. 4 > 0 then Just (x, y + 1) else Nothing
-          pW = if parents .&. 8 > 0 then Just (x + 1, y) else Nothing
-      parentTiles <- mapM (stParents parentsArr) $ toInt <$> catMaybes [pS, pE, pN, pW]
-      return $ y * memorySize + x : concat parentTiles
+part2 :: Text -> Byte
+part2 input =
+  let bs = parseInput input
+   in findFirstBadByte bs
 
 day18 :: Text -> IO (String, String)
 day18 input = do
-  let bs = parseInput input
-      mem = simulate bs 1024
-      txt = display mem
-  mapM_ (putStrLn . T.unpack) txt
-  return (show $ shortestPath mem, "N/A")
+  return (show $ part1 input, show $ part2 input)
